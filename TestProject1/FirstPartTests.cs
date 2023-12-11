8ee959bd5e57a0958da2eaa546198a298f7690cc
KaunivaJulia <151840241+KaunivaJulia@users.noreply.github.com>
2023-12-11 23:32:44 +0300

    Update FirstPartTests.cs

--- TestProject1/FirstPartTests.cs
+++ TestProject1/FirstPartTests.cs
@@ -29,44 +29,19 @@ namespace TestProject1
             int B = 6;
             Assert.Equal(firstPart.GetCountOfArrayBetweenAB(A,B), firstPart.Vector.Where(i => i <= B & i>=A).Count());
         }
+    
         
         [Fact]
-        public void IsSumValid()
-        {
-            var firstPart = new FirstPart(100);
-            var arr = firstPart.Vector.ToArray();
-
-            var maxIndex = Array.IndexOf(arr, arr.Max());
-
-            int sum = 0;
-            for (int i = maxIndex + 1; i < arr.Length; i++)
-            {
-
-                sum += arr[i];
-
-            }
+       public void IsSortedByAscending()
+{
+    var firstPart = new FirstPart(100);
+    firstPart.Sort();
+    for (int i = 0; i < firstPart.Vector.Count - 1; i++)
+    {
+        Assert.True(Math.Abs(firstPart.Vector[i]) <= Math.Abs(firstPart.Vector[i + 1]));
+    }
 
 
-            var prov = firstPart.GetSumAfterMax();
-
-            //var mul = arr.Skip(minIndex + 1).Take(maxIndex - minIndex - 1).ToList();
-            if (maxIndex == arr.Length - 1)
-            {
-                Assert.Equal(0, prov);
-            }
-            else
-            {
-                Assert.Equal(, prov);
-            }
-        }
-
-        
-        [Fact]
-        public void IsSortedByAscending()
-        {
-            
-        }
-
-
+}
     }
 }
